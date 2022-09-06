d3ed3af91c061730919540d174319bb03ba6e977
Lee <lcanegaly@gmail.com>
2022-09-05 18:15:08 -0700

    Fixed missing parenthesis in Expansibility function. Formatting.

--- orificeflow.cpp
+++ orificeflow.cpp
@@ -1,63 +1,61 @@
 #include "orificeflow.h"
 
-double orificeFlow::InchToMeter(double inch)
-{
-    return inchToMeter * inch;
+double orifice_flow::InchToMeter(double inch) { return inch_to_meter * inch; }
+
+double orifice_flow::FToC(double fahrenheit) {
+  return (fahrenheit - 32) * 5 / 9;
 }
 
-double orificeFlow::FToC(double fahrenheit)
-{
-    return (fahrenheit - 32) * 5/9;
+double orifice_flow::M3sToCfm(double flow_rate) {
+  return cfm_per_m3s * flow_rate;
 }
 
-double orificeFlow::M3sToCfm (double flowRate)
-{
-    return cfmPerCubicMeterSecond * flowRate;
+double orifice_flow::KGsToM3s(double mass_flow_kg_s, double air_density_kg_m3) {
+  return mass_flow_kg_s / air_density_kg_m3;
 }
 
-double orificeFlow::KGsToM3s(double massFlowKGs, double airDensityKgM3)
-{
-    return massFlowKGs / airDensityKgM3; 
+double orifice_flow::OrificeMassFlowKGs(
+    double coefficient_of_discharge, double orifice_ratio, double expansibility,
+    double orifice_diamter_m, double fluid_density_kg_m3, double delta_pa) {
+  return (coefficient_of_discharge / sqrt(1 - pow(orifice_ratio, 4))) *
+         (expansibility * (M_PI / 4) * pow(orifice_diamter_m, 2)) *
+         sqrt(2 * delta_pa * fluid_density_kg_m3);
 }
 
-double orificeFlow::OrificeMassFlowKGs(double coefficientOfDischarge, double orificeRatio, 
-    double expansibility, double orificeDiamterM, double fluidDensityKgM3, double deltaPa)
-{
-    return (coefficientOfDischarge / sqrt(1 - pow(orificeRatio, 4))) 
-        * (expansibility * (M_PI / 4) * pow(orificeDiamterM, 2)) * sqrt(2 * deltaPa * fluidDensityKgM3);
+double orifice_flow::AirPressureKPa(double elevation) {
+  // ToDo calculate atmospheric pressure from elevation
+  return 101.325;
 }
 
-double orificeFlow::AirPressureKPa(double elevation)
-{
-    //ToDo calculate atmospheric pressure from elevation
-    return 101.325;
+double orifice_flow::AirDensityKgM3(double temp_celcius,
+                                    double atmospheric_pressure_kpa) {
+  // ToDo calculate density
+  // For now - At 20 °C and 101.325 kPa, dry air has a density of 1.2041 kg/m3.
+  return 1.2041;
 }
 
-double orificeFlow::AirDensityKgM3(double tempCelcius, double atmosphericPressureKPa)
-{ 
-    //ToDo calculate density
-    //For now - At 20 °C and 101.325 kPa, dry air has a density of 1.2041 kg/m3.
-    return 1.2041;
+double orifice_flow::OrificeFlowCfm(double elevation_ft,
+                                    double orifice_diameter_inch,
+                                    double air_temp_f, double delta_pa,
+                                    double coefficient) {
+  double air_density = AirDensityKgM3(FToC(air_temp_f), AirPressureKPa(0));
+
+  double flow_kg_s = OrificeMassFlowKGs(
+      coefficient, orifice_to_pipe_ratio, Expansibility(),
+      InchToMeter(orifice_diameter_inch), air_density, delta_pa);
+
+  return M3sToCfm(KGsToM3s(flow_kg_s, air_density));
 }
 
-double orificeFlow::OrificeFlowCfm(double elevationFt, double orificeDiameterInch, double airTempF, double deltaPa, double coefficient)
-{
-    double airDensity = AirDensityKgM3( FToC(airTempF), AirPressureKPa(0) );
-    
-    double flowKgs = OrificeMassFlowKGs(coefficient, orificeToPipeRatio, Expansibility(), 
-        InchToMeter(orificeDiameterInch), airDensity, deltaPa);
-    
-    return M3sToCfm(KGsToM3s(flowKgs, airDensity));
-}
-
-//ISO 5167 Expansibility calc
-double orificeFlow::Expansibility()
-{
-    //ϵ= 1 − (0.351 + 0.256β^4 + 0.93β^8)⋅[1−(pout/pin)^1/κ]
-    //β = narrowing ratio of pipe to orifice
-    //κ = heat capacity ratio = using estimated val of 1.3
-    //pin intake pressure, pout discharge pressure (50pa used as constant)
-    const double pin = 101350;
-    const double pout = 101300;
-    return 1 - (0.351 + 0.256 * pow(orificeToPipeRatio, 4) + 0.93 * pow(orificeToPipeRatio, 8) * (1 - pow((pout/pin), (1/1.3)) ));
+// ISO 5167 Expansibility calc
+double orifice_flow::Expansibility() {
+  //ϵ= 1 − (0.351 + 0.256β^4 + 0.93β^8)⋅[1−(pout/pin)^1/κ]
+  //β = narrowing ratio of pipe to orifice
+  //κ = heat capacity ratio = using estimated val of 1.3
+  // pin intake pressure, pout discharge pressure (50pa used as constant)
+  const double pa_in = 101350;
+  const double pa_out = 101300;
+  return 1 - (0.351 + 0.256 * pow(orifice_to_pipe_ratio, 4) +
+              0.93 * pow(orifice_to_pipe_ratio, 8)) *
+                 (1 - pow((pa_out / pa_in), (1 / 1.3)));
 };
--- orificeflow.h
+++ orificeflow.h
@@ -5,28 +5,27 @@
 #define M_PI 3.14159265358979323846
 #endif
 
-namespace orificeFlow
-{
+namespace orifice_flow {
+constexpr double inch_to_meter = 0.0254;
+constexpr double cfm_per_m3s = 2118.8799727597;
+constexpr double orifice_to_pipe_ratio = 0.001;
 
-constexpr double inchToMeter = 0.0254;
-constexpr double cfmPerCubicMeterSecond = 2118.8799727597;
-constexpr double orificeToPipeRatio = 0.001;
+// ISO 5167 Orifice plate flow Equation.
+double OrificeMassFlowKGs(double coefficient_of_discharge, double orifice_ratio,
+                          double expansibility, double orifice_diamter_m,
+                          double fluid_density_kg_m3, double delta_pa);
 
-//ISO 5167 Orifice plate flow Equation.
-double OrificeMassFlowKGs(double coefficientOfDischarge, double orificeRatio, 
-    double expansibility, double orificeDiamterM, double fluidDensityKgM3, double deltaPa);
-
-//More convenient version of Orifice flow function for imperial units, converts mass flow to volume flow.
-double OrificeFlowCfm(double elevationFt, double orificeDiameterInch, double airTempF, double deltaPa, double coefficient);
-
+// More convenient version of Orifice flow function for imperial units, converts
+// mass flow to volume flow.
+double OrificeFlowCfm(double elevation_ft, double orifice_diameter_inch,
+                      double air_temp_f, double delta_pa, double coefficient);
 double InchToMeter(double inch);
 double FToC(double fahrenheit);
-double M3sToCfm (double flowRate);
-double KGsToM3s(double massFlowKGs, double airDensityKgM3);
+double M3sToCfm(double flow_rate);
+double KGsToM3s(double mass_flow_kg_s, double air_density_kg_m3);
 double AirPressureKPa(double elevation);
-double AirDensityKgM3(double tempCelcius, double atmosphericPressureKPa);
+double AirDensityKgM3(double temp_celcius, double atmospheric_pressure_kpa);
 
-//ISO 5167 Expansibility calc
+// ISO 5167 Expansibility calc
 double Expansibility();
-
-}
+}  // namespace orifice_flow
